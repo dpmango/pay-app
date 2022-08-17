1cc7f4e65efc06c964d6fc94f5de8e5b3f8c0246
Sergey Khmelevskoy <xs290@me.com>
2022-08-17 12:18:22 +0300

    add swiper

--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { observer } from 'mobx-react-lite';
 import cns from 'classnames';
 
@@ -8,6 +8,7 @@ import { UiStoreContext } from '@store';
 
 import { Story } from '@c/Stories';
 import st from './Stories.module.scss';
+import 'swiper/css';
 
 const stories = [
   {
@@ -17,6 +18,13 @@ const stories = [
     title: 'Если затеяли ремонт в доме',
     imgPosition: 'center right',
   },
+  {
+    id: 2,
+    img: '/img/stories-1.jpg',
+    img2x: '/img/stories-1@2x.jpg',
+    title: '2 Если затеяли ремонт в доме',
+    imgPosition: 'center right',
+  },
 ];
 const Stories = observer(({ className }) => {
   const uiContext = useContext(UiStoreContext);
@@ -24,9 +32,14 @@ const Stories = observer(({ className }) => {
   return (
     <>
       <section className={cns(st.container, className)}>
-        <div className="container">
-          {stories && stories.map((story) => <Story {...story} key={story.id} />)}
-        </div>
+        <Swiper spaceBetween={14} slidesPerView={'auto'}>
+          {stories &&
+            stories.map((story) => (
+              <SwiperSlide key={story.id}>
+                <Story {...story} />
+              </SwiperSlide>
+            ))}
+        </Swiper>
       </section>
     </>
   );
